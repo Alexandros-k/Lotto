496ea7c026c796149fe9321fa40a59284dd543d4
Alexandros Konstantinidis <Alexandros.Konstantinidis@eurodyn.com>
2020-01-23 11:28:31 +0200

    first commit

---
+++ src/JokerDisplay.java
@@ -0,0 +1,8 @@
+
+public class JokerDisplay extends Display {
+
+	protected void display(int lottoBallNumber) {
+		String message = "Joker Number"+" ";
+		System.out.println(message + lottoBallNumber);		
+	}	
+}
